6260e40ad3e7de86d42d0392ed9122e5b0bca87b
Matt Hough <matt@hough.tv>
2017-09-27 03:49:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(num)
   return false unless num > 0
   return true if num == 2
-  return false if num == 1
+  return false if num == 1 || num.even?
 
   (3..num/2).step(2).to_a.each do |check|
     return false if num % check == 0
