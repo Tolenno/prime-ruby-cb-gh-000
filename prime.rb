9ffe8a79013c4f1d5af06d20d8c90bb747bf10d4
Matt Hough <matt@hough.tv>
2017-09-27 03:39:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,4 @@
 def prime?(num)
   return false unless num > 0
-
 end
 
-
-1. Get int variable
-2. Generate random integer less than int variable
-3. Is greatest common divisor of random number and int 1?
-4. If not 1, then they share a common factor and int variable is composite
-5. If is 1, ask if random integer to the power of int variable - 1 mod int variable == 1?
-6. If not, we have a witness int variable is composite
-7. If yes, int variable is composite
-
-Fools
