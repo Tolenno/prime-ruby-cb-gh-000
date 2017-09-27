52392b77d103d684b0c41e2be2558b8bf4fc09ee
Matt Hough <matt@hough.tv>
2017-09-27 03:49:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  return false unless num > 0
+  return true if num == 2
+  return false if num == 1 || num.even?
+
+  (3..num/2).step(2).to_a.each do |check|
+    return false if num % check == 0
+  end
+  return true
+end
